a711c2e5c15924abdc24f02b2ddb3fdaa2cbed65
Cole Mickens <cole.mickens@gmail.com>
2017-12-19 09:13:48 -0800

    wip, things are borked

--- x-start-cluster.sh
+++ x-start-cluster.sh
@@ -10,15 +10,20 @@ sudo systemctl daemon-reload
 
 sudo systemctl start kubelet
 sudo systemctl enable kubelet
-sudo kubeadm init
+sudo kubeadm init --pod-network-cidr=10.0.0.0/16
 
-mkdir -p $HOME/.kube                           
-sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config                                        
+mkdir -p $HOME/.kube
+sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubectl apply -f http://docs.projectcalico.org/v2.3/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
+kubectl apply -f https://docs.projectcalico.org/v2.6/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+wget https://docs.projectcalico.org/v2.6/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml -O /tmp/calico.yaml
+sed -i 's|192.168.0.0/16|10.0.0.0/16|g' /tmp/calico.yaml
+kubectl apply -f /tmp/calico.yaml
+
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
+# TODO: This is lazy/tacky:
 kubectl create clusterrolebinding permissive-binding \
   --clusterrole=cluster-admin \
   --user=admin \
